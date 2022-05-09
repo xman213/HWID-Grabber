b9c93d88b150d69070ff09dc0bfa4a0df3557833
xman <82530118+xman213@users.noreply.github.com>
2022-05-09 11:56:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from pystyle import Colorate, Anime, Colors
+import subprocess
+import requests
+import pystyle
+import time
+import sys
+import os
 
+hwid = str(str(subprocess.check_output('wmic csproduct get uuid')).strip().replace(r"\r", "").split(r"\n")[1].strip())
+prefix = "YOUR_PREFIX_HERE."+hwid
+
+print(Colorate.Horizontal(Colors.red_to_blue, (prefix))
+time.sleep(30)
